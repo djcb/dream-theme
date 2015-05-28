925bbee48ede496802be893a149ef2bcfc3c7a94
Evgeny Roubinchtein <zhenya1007@gmail.com>
2015-05-28 12:41:13 -0700

    add autoload cookie

--- dream-theme.el
+++ dream-theme.el
@@ -220,4 +220,11 @@
 
     ))
 
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'dream)
